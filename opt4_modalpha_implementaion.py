d6f9ba1b4165ae0737fbf61954a4065c35343512
ali <ali@ali.ali>
2018-03-26 11:40:24 +0300

    back to commti

---
+++ opt4_modalpha_implementaion.py
@@ -0,0 +1,276 @@
+#!/usr/bin/env python
+
+'''
+Implementation of the Ahmed et.al. p+q search
+
+psudocode:
+
+	if n mod 10 == 1 then:
+		if n mod 100 == 01 then:
+			if n+1 mod 3 == 0 then:
+				a1 = 300
+				b1 = 102
+				a2 = 300
+				b2 = 198
+				a3 = 60
+				b3 = 30
+				for k:
+					check_eq(a1,b1)
+					check_eq(a2,b2)
+					check_eq(a3,b3)
+
+		if n mod 100 == 21 then:
+		if n mod 100 == 41 then:
+		if n mod 100 == 61 then:
+		if n mod 100 == 81 then:
+
+		if n mod 100 == 11 then:
+		if n mod 100 == 31 then:
+		if n mod 100 == 51 then:
+		if n mod 100 == 71 then:
+		if n mod 100 == 91 then:
+
+'''
+
+import sys
+import subprocess
+import json
+from timeit import default_timer
+from math import ceil
+from math import sqrt
+from gmpy2 import mpz
+from gmpy2 import is_square
+from gmpy2 import isqrt
+
+#Global
+
+#num_primes = 2  # only 2 primes are suppoorted for now
+#ret = subprocess.call(["./mod_prodp_gen.py"])
+
+n=sys.argv[1]
+
+prodfp = open('mod_prodp.json','r')
+pfp = open('mod_p.json','r')
+mod_p = json.load(pfp)
+mod_prodp = json.load(prodfp)
+
+np1_modp_str = ''
+for p in mod_p.keys():
+    np1_modp_str += ' , ' + str((int(n)+1) % int(p))
+    print(np1_modp_str)
+
+np1_modp_str = np1_modp_str[3:]  #Correctng first element
+permlst = mod_prodp[np1_modp_str] # the list we are looking for
+
+acc=1
+for i in mod_p.keys():
+    acc *= int(i)
+p_prod = acc
+
+pos = {}
+#
+
+def check_eq(r,n):
+    #r = a * k + b
+    interm = r**2 - 4*n
+    #res = math.sqrt(interm)
+    is_res = is_square(mpz(interm))
+    #fplot.write(str(k) + "  " + str(r) + "  " + str(r % 7) + "  " + str(r % 11) + "  " + str(interm) + '    ' +  '\n')
+    #print interm
+    if (is_res):
+        print("\n----------------------------------------------")
+        print('n = '+str(n) + '4n = ' + str(4*n))
+        print('n + 1 mod 7 = ' + str( (n+1) % 7))
+        print('n + 1 mod 11 = ' + str( (n+1) % 11))
+        print('n + 1 mod 13 = ' + str( (n+1) % 13))
+        print('- interm = ' + str(interm))
+        res = isqrt(interm)
+        print("result p-q= " + str(res) + " at r = " + str(r))
+        p = (r + res)/2
+        q = n / p
+        print("p = " + str(p))
+        print("q = " + str(q))
+        #print("k = " + str(k))
+        if (p.is_integer() & q.is_integer()):
+            print("DONE!")
+            return(1)
+    return(0)
+
+def check_modlist(a,b,k,index,n):
+
+    r = a * k + b
+
+    if (r % p_prod) in permlst:
+        pos.setdefault(index, []).append(k)
+        ret_mod = check_eq(r,n)
+        return(ret_mod)
+    return(0)
+
+def check_pos(a,b,k,n):
+
+    r = a * k + b
+
+    ret_pos = check_eq(r,n)
+    return(ret_pos)
+
+    # record results
+#    for i in range(1,l+1):
+#    	fplot.append(open('plot' + str(i), 'w+'))
+
+
+def prep_check(s1,s2,s3,n):
+    fp = open('eq.json', 'r')
+    eq = json.load(fp)
+    l = len(eq[s1 + s2 + '|' + s3]) / 2
+    a = []; b = []; g = []; e = []; fplot = []
+
+    print(n)
+    for i in range(1,int(l)+1):
+        #fplot.append(open('plot' + str(i), 'w+'))
+        a.append(mpz(eq[s1 + s2 + '|' + s3]['a'+ str(i)]))
+        b.append(mpz(eq[s1 + s2 + '|' + s3]['b'+ str(i)]))
+        g.append(int(ceil((2 * ceil(sqrt(n)) - b[i-1]) / a[i-1])))
+        e.append(int(ceil((sqrt(n**2 + 4*n - 1) - b[i-1]) / a[i-1])))
+
+    fp.close()
+    gs = sorted(enumerate(g) , key = lambda i:i[1])
+    print (gs)
+    print (e)
+    n = mpz(n)
+
+    # Prepare position list
+
+    m_e = min(e)
+    m_g = min(g)
+    while (m_g <= (m_g + p_prod)):
+        m_g += 1
+        i = 0
+        while (i < l):
+            ret_modlist = check_modlist(a[i],b[i],g[i],i,n)
+            if ret_modlist == 1 :
+                return
+            g[i] += 1
+            if (g[i] >= m_e):
+                ind_e = e.index(m_e)
+                del e[ind_e]
+                del g[ind_e]
+                del a[ind_e]
+                del b[ind_e]
+                l = len(a)
+                print(" deleting ind " + str(ind_e) + " e now : "),
+                print(e)
+                if (l == 0):
+                    print("Ending...")
+                    exit()
+                m_e = min(e)
+            i += 1
+
+    # Complete the search using position list
+
+    m = 2
+    while (m_g <= m_e):
+        for pos_i in range(0,p_prod):
+            eq_i = 0
+            while (eq_i < l):
+                km = m * p_prod + pos[eq_i][pos_i]
+                ret_pos = check_pos(a[eq_i],b[eq_i],km,n)
+                if ret_pos == 1 :
+                    return
+                g[eq_i] += 1
+                if (g[eq_i] >= m_e):
+                    ind_e = e.index(m_e)
+                    del e[ind_e]
+                    del g[ind_e]
+                    del a[ind_e]
+                    del b[ind_e]
+                    l = len(a)
+                    print(" deleting ind " + str(ind_e) + " e now : "),
+                    print(e)
+                    if (l == 0):
+                        print("Ending...")
+                        exit(1)
+                    m_e = min(e)
+                i += 1
+        m += 1
+
+#        m = 2
+#        cnt = 0
+#	while (m_g <= m_e):
+#		i = 0
+#		while (i < l):
+#			check_pos(a[i],b[i],g[i],m,i,n)
+#			g[i] += 1
+#			if (g[i] >= m_e):
+#				ind_e = e.index(m_e)
+#				del e[ind_e]
+#				del g[ind_e]
+#				del a[ind_e]
+#				del b[ind_e]
+#				l = len(a)
+#				print " deleting ind " + str(ind_e) + " e now : ",
+#				print e
+#				if (l == 0):
+#					print "Ending..."
+#					exit()
+#				m_e = min(e)
+#			i += 1
+
+    return
+
+def start(n):
+    n_100 = int(str(n)[-3])
+    n_10 = int(str(n)[-2])
+    n_1 = int(str(n)[-1])
+
+    if ((n+1) % 3 == 0):
+        s1 = '3'
+    elif ((n+1) % 3 != 0):
+        s1 = '3!'
+    else:
+        print ("Error")
+        exit(1)
+
+    if (n_1 == 3 or n_1 == 7):
+        s3 = '0' + str(n_1)
+        if (n_10 % 2 != 0):
+            s2 = ',4'
+        elif (n_10 % 2 == 0):
+            s2 = ',4!'
+        else:
+            print ("Error")
+            exit(1)
+
+    elif (n_1 == 1 or n_1 == 9):
+        s3 = str(n_10) + str(n_1)
+        if (n_10 % 2 != 0):
+            if (n_100 % 2 != 0):
+                s2 = ',4'
+            elif (n_100 % 2 == 0):
+                s2 = ',4!'
+            else:
+                print ("Error")
+                exit(1)
+        elif (n_10 % 2 == 0):
+            s2 = ''
+        else:
+            print ("Error")
+            exit(1)
+    else:
+        print ("Error")
+        exit(1)
+    prep_check(s1,s2,s3,n)
+    return
+
+
+def main():
+    tfp = open('time_opt4.log','a+')
+    n = int(sys.argv[1])
+    start_time = default_timer()
+    start(n)
+    Time = str(default_timer() - start_time)
+    print( "Time: " +  Time )
+    tfp.write("n = " + str(n) + "   length (digits): " + str(len(str(n))) + "   Time: " + Time + '\n')
+    exit(0)
+
+if __name__ == '__main__':
+    main()
