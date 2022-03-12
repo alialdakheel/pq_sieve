78334a1c92f273b065c7afcc710f6e5ba88b5cf9
ali <ali@ali.ali>
2022-03-12 13:50:16 -0500

    Updated example generation script

--- utils/gen_n.sh
+++ utils/gen_n.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-n=$1 # Number of bits
-p=$(openssl prime -generate -bits $n)
-q=$(openssl prime -generate -bits $n)
+N=$1 # Number of bits
+N_2=$(echo "$N / 2" | bc)
+p=$(openssl prime -generate -bits $N)
+q=$(openssl prime -generate -bits $N)
+p2=$(openssl prime -generate -bits $[ N - N_2 ])
+q2=$(openssl prime -generate -bits $[ N + N_2 ])
 n=$(echo "$p * $q" | bc)
-np1_mod3=$(echo "( $n + 1 ) %  3" | bc)
-np1_mod7=$(echo "( $n + 1 ) %  7" | bc)
-echo "$p * $q  =  $n ,	$np1_mod3,	$np1_mod7"
+ppq=$(echo "$p + $q" | bc)
+pmq=$(echo "$p - $q" | bc)
+ppq2=$(echo "$p2 + $q2" | bc)
+pmq2=$(echo "$p2 - $q2" | bc)
+np1_mod3=$(echo "( $n ) %  3" | bc)
+np1_mod7=$(echo "( $n ) %  7" | bc)
+echo "n = p*q = $p '*' $q  =  $n ,	nmod3= $np1_mod3, nmod7= $np1_mod7, p+q= $ppq, p-q= ${pmq#-}"
+echo "n = p2*q2 = $p2 '*' $q2  =  $n,  p+q= $ppq2, p-q= ${pmq2#-}"
