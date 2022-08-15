ef05340361a8970bdb726d72865def318c28c81c
ali <ali@ali.ali>
2022-08-15 07:12:24 -0400

    Added x with adjusted primes (same as in paper)

--- primorial.py
+++ primorial.py
@@ -9,12 +9,16 @@ m = list(range(1, limit + 1))
 primes = load_primes("primes.txt", limit=limit) 
 
 primorial = [reduce(mul, primes[:l]) for l in range(1,limit + 1)]
-bound_b2 = [int(2**(x*math.log2(x))) for x in m]
-bound_be = [int(math.e**(x*math.log2(x))) for x in m]
-bound_b3 = [int(3**(x*math.log2(x))) for x in m]
+adj_primes = [primes[0]**6, primes[1]**2, primes[2]**2] + primes[3:]
+x = [reduce(mul, adj_primes[:l]) for l in range(1,limit + 1)]
+
+bound_b2 = [2**(x*math.log2(x)) for x in m]
+bound_be = [math.e**(x*math.log2(x)) for x in m]
+bound_b3 = [3**(x*math.log2(x)) for x in m]
 
 res_dict = {
-    "primorial" : primorial,
+    "primorial" : map(float, primorial),
+    "x" : map(float, x),
     "bound_b2" : bound_b2,
     "bound_be" : bound_be,
     "bound_b3" : bound_b3,
